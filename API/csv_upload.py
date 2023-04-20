bc4bdd5d7cbfc9e0c75151bcdf4e35d00178cc31
Ash <ashwinkrishnan54@gmail.com>
2023-04-20 11:12:43 +0530

    Data Upload to postgresql 20/04/24-11:12

--- API/csv_upload.py
+++ API/csv_upload.py
@@ -1,15 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import postgres_settings
-import pandas as pd
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from config import postgres_settings
+# import pandas as pd
 
-SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{postgres_settings.database_username}:{postgres_settings.database_password}@{postgres_settings.database_hostname}:{postgres_settings.postgres_database_port}/{postgres_settings.database_name}'
+# SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{postgres_settings.database_username}:{postgres_settings.database_password}@{postgres_settings.database_hostname}:{postgres_settings.postgres_database_port}/{postgres_settings.database_name}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+# SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
-with SessionLocal() as session:
-    df = pd.read_csv('../data/fraud.csv') 
-    df.to_sql('transactions', con=engine, if_exists='append',index=False)
+# with SessionLocal() as session:
+#     df = pd.read_csv('../data/fraud.csv') 
+#     df.to_sql('transactions', con=engine, if_exists='append',index=False)
